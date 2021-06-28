f298b333e346eb9602feb80242aed4451e1d8c5d
zhaodongyang <zhaodongyang@inspur.com>
2021-06-28 12:48:09 +0800

    commit new spring project

---
+++ src/test/java/com/bigdata/hdinsight/HdinsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdata.hdinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdinsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
